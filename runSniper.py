a661dcf8eb99cf71c253157ce90801b32ea815b9
ginajsalib <ginajemy@gmail.com>
2025-01-04 08:14:07 +0000

    fix l3 cache naming

--- runSniper.py
+++ runSniper.py
@@ -4,12 +4,12 @@ from itertools import product
 
 # Define the parameter options
 cache_sizes_l2 = [256, 512, 1024]
-cache_sizes_l2MB = [4, 8, 12, 16]
+cache_sizes_l3MB = [4, 8, 12, 16]
 prefetchers = ["none", "simple", "ghb"]
 branch_predictor_sizes = [512, 1024, 2048, 4096]
 
 # Function to run the command with specified options
-def run_sniper_command(directory, cache_size_l2, cache_size_l2MB, prefetcher, branch_predictor):
+def run_sniper_command(directory, cache_size_l2, cache_size_l3MB, prefetcher, branch_predictor):
     os.chdir("/root/benchmarks")
     command = [
         './run-sniper',
@@ -21,7 +21,7 @@ def run_sniper_command(directory, cache_size_l2, cache_size_l2MB, prefetcher, br
     
     # Add the perf_model configurations using .format() for compatibility with Python 3.4
     command.append('perf_model/l2_cache/cache_size={}'.format(cache_size_l2))
-    command.append('perf_model/l2_cache/cache_size={}MB'.format(cache_size_l2MB))
+    command.append('perf_model/l2_cache/cache_size={}MB'.format(cache_size_l3MB))
     command.append('perf_model/l2_cache/prefetcher={}'.format(prefetcher))
     command.append('perf_model/branch_predictor/size={}'.format(branch_predictor))
     
@@ -32,13 +32,13 @@ def run_sniper_command(directory, cache_size_l2, cache_size_l2MB, prefetcher, br
 # Main function to generate configurations and run commands
 def main():
     # Create all combinations of the parameters
-    configurations = product(cache_sizes_l2, cache_sizes_l2MB, prefetchers, branch_predictor_sizes)
+    configurations = product(cache_sizes_l2, cache_sizes_l3MB, prefetchers, branch_predictor_sizes)
     
     # Iterate through each combination and run the command
-    for cache_size_l2, cache_size_l2MB, prefetcher, branch_predictor in configurations:
+    for cache_size_l2, cache_size_l3MB, prefetcher, branch_predictor in configurations:
         # Create a directory name based on the parameters
-        directory = '/root/snipersim_framework/config_l2_{}_l2MB_{}_prefetch_{}_branch_{}'.format(
-            cache_size_l2, cache_size_l2MB, prefetcher, branch_predictor
+        directory = '/root/snipersim_framework/config_l2_{}_l3MB_{}_prefetch_{}_branch_{}'.format(
+            cache_size_l2, cache_size_l3MB, prefetcher, branch_predictor
         )
         
         # Ensure the directory exists (this is for logging, data collection, etc.)
@@ -46,7 +46,7 @@ def main():
             os.makedirs(directory)
         
         # Run the sniper command with the given configuration
-        run_sniper_command(directory, cache_size_l2, cache_size_l2MB, prefetcher, branch_predictor)
+        run_sniper_command(directory, cache_size_l2, cache_size_l3MB, prefetcher, branch_predictor)
 
 # Run the script
 if __name__ == "__main__":
