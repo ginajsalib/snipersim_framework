baf065c45d4fff7d7e42453d659aa80bf5c58e90
ginajsalib <ginajemy@gmail.com>
2025-06-16 18:41:37 +0000

    added instruction interval 0.5M and max inst 1B

--- runSniperWithCfg.sh
+++ runSniperWithCfg.sh
@@ -8,6 +8,7 @@ cache_sizes_l2=(256)
 cache_sizes_l3MB=(8192)
 prefetchers=("none")
 branch_predictor_sizes=(512 1024 2048 4096)
+#branch_predictor_sizes=(512)
 benchmark="barnes"
 
 NUM_CORES=2
@@ -40,7 +41,7 @@ num_entries = ${bp1}
 EOF
 
           # Construct the output directory name
-          directory="/root/snipersim_framework/config_l2_${l2}_l3MB_${l3}_prefetch_${prefetch}_branch_${bp0}-${bp1}_${benchmark}"
+          directory="/root/snipersim_framework/config_l2_${l2}_l3MB_${l3}_prefetch_${prefetch}_branch_${bp0}-${bp1}_${benchmark}-intervals"
           mkdir -p "$directory"
 
           # Build benchmark name
@@ -54,12 +55,14 @@ EOF
             "-c" "gainestown"
             "-c" "rob"
             "-c" "core0,core1"
+            "-s" "periodicins-stats.py:500000"
             "-d" "$directory"
             "-g" "perf_model/l2_cache/cache_size=${l2}"
             "-g" "perf_model/l3_cache/cache_size=${l3}"
             "-g" "perf_model/l2_cache/prefetcher=${prefetch}"
             "-g" "perf_model/branch_predictor/num_ways=4"
-          )
+            "-g" "general/max_instructions=1000000000" 
+         )
 
           echo "Running: ${cmd[*]}"
           "${cmd[@]}"
