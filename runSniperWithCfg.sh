887441e66016228428fd60cb672a9b0a1c8920b6
ginajsalib <ginajemy@gmail.com>
2026-03-26 08:45:44 +0000

    adedd multiple l2 l3 sizes and prefetcher per core

--- runSniperWithCfg.sh
+++ runSniperWithCfg.sh
@@ -4,23 +4,24 @@
 cd /root/benchmarks || exit 1
 
 # Configuration parameters
-cache_sizes_l2=(256)
-cache_sizes_l3MB=(8192)
-prefetchers=("none", "simple")
-#branch_predictor_sizes=(512 1024 2048 4096)
-branch_predictor_sizes=(512)
+cache_sizes_l2=(512 1024)
+cache_sizes_l3MB=(4096 12288 16384)
+prefetchers=("simple" "none")
+branch_predictor_sizes=(512 1024 2048 4096)
+#branch_predictor_sizes=(512)
 benchmark="barnes"
 
 NUM_CORES=2
 DISPATCH_WIDTH=4
-CFG_DIR="/root/snipersim_framework"
+CFG_DIR="/root/benchmarks"
 
 # Loop through all combinations
 for l2 in "${cache_sizes_l2[@]}"; do
   for l3 in "${cache_sizes_l3MB[@]}"; do
-    for prefetch in "${prefetchers[@]}"; do
-      for bp0 in "${branch_predictor_sizes[@]}"; do
-        for bp1 in "${branch_predictor_sizes[@]}"; do
+    for prefetch0 in "${prefetchers[@]}"; do
+      for prefetch1 in "${prefetchers[@]}"; do  
+        for bp0 in "${branch_predictor_sizes[@]}"; do
+          for bp1 in "${branch_predictor_sizes[@]}"; do
 
           # Create config files for core0 and core1
           cat <<EOF > "$CFG_DIR/core0.cfg"
@@ -30,6 +31,8 @@ dispatch_width = ${DISPATCH_WIDTH}
 [perf_model/branch_predictor]
 num_entries = ${bp0}
 
+[perf_model/l2_cache]
+prefetcher = ${prefetch0}
 EOF
 
           cat <<EOF > "$CFG_DIR/core1.cfg"
@@ -38,6 +41,8 @@ dispatch_width = ${DISPATCH_WIDTH}
 
 [perf_model/branch_predictor]
 num_entries = ${bp1}
+[perf_model/l2_cache]
+prefetcher = ${prefetch1}
 EOF
 
           # Construct the output directory name
@@ -60,7 +65,7 @@ EOF
             "-d" "$directory"
             "-g" "perf_model/l2_cache/cache_size=${l2}"
             "-g" "perf_model/l3_cache/cache_size=${l3}"
-            "-g" "perf_model/l2_cache/prefetcher=${prefetch}"
+     
             "-g" "perf_model/branch_predictor/num_ways=4"
             "-g" "general/max_instructions=1000000000" 
             "-g" "perf_model/l2_cache/prefetcher/prefetch_on_prefetch_hit=true"
@@ -72,7 +77,7 @@ EOF
 
           echo "Running: ${cmd[*]}"
           "${cmd[@]}"
-
+         done
         done
       done
     done
